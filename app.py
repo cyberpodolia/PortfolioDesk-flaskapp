c04b1db6d6a590ebc7887016a30b8f39ab42f05b
Yondr <iamyondr@gmail.com>
2026-02-07 11:55:22 +0200

    added icon

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory
 import os
 import sys
 import requests
@@ -47,6 +47,14 @@ BOT_TOKEN, CHAT_ID = load_telegram_config()
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon"
+    )
 
 @app.route('/api/contact', methods=['POST'])
 def contact():
